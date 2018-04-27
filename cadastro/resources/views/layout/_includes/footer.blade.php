ea73cb3782ecf5af958360860fbb35c572ae5267
matfatjoe <matheus.furquim@wdstech.com.br>
2018-04-27 19:18:49 -0300

    Projeto cadastro

---
+++ cadastro/resources/views/layout/_includes/footer.blade.php
@@ -0,0 +1,13 @@
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+    Materialize.updateTextFields();
+    $(document).ready(function(){
+      $('sidenav').sidenav();
+    });
+  });
+</script>
+</body>
+</html>
---
+++ cadastro/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/', ['as'=>'site.home', 'uses'=>'Site/HomeController@index']);
+
+Route::get('/admin/usuarios', ['as'=>'admin.cursos'], ['uses'=>'Admin/CursoController@index']);
+Route::get('/admin/usuarios/adicionar', ['as'=>'admin.cursos.adicionar'], ['uses'=>'Admin/CursoController@adicionar']);
+Route::post('/admin/usuarios/salvar', ['as'=>'admin.cursos.salvar'], ['uses'=>'Admin/CursoController@salvar']);
+Route::get('/admin/usuarios/editar/{id}', ['as'=>'admin.cursos.editar'], ['uses'=>'Admin/CursoController@editar']);
+Route::put('/admin/usuarios/atualizar/{id}', ['as'=>'admin.cursos.atualizar'], ['uses'=>'Admin/CursoController@atualizar']);
+Route::get('/admin/usuarios/deletar/{id}', ['as'=>'admin.cursos.deletar'], ['uses'=>'Admin/CursoController@deletar']);
+
+/*Usuario*/
